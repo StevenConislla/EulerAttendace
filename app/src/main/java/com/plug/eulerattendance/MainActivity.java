5207359d813c9cf7f32a4dfc37cc3ce0e8579fff
StevenConislla <oskarconislla20@gmail.com>
2017-12-08 11:08:23 -0500

    Primer commit

--- app/src/main/java/com/plug/eulerattendance/MainActivity.java
+++ app/src/main/java/com/plug/eulerattendance/MainActivity.java
@@ -1,14 +1,9 @@
 package com.plug.eulerattendance;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -16,16 +11,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.plug.eulerattendance.pruebas.OnSwipeTouchListener;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    @BindView(R.id.contenedor)
+    LinearLayout contenedor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -37,6 +42,31 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        contenedor.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this) {
+                                          public void onSwipeTop() {
+                                              Toast.makeText(MainActivity.this, "arriba", Toast.LENGTH_SHORT).show();
+                                          }
+
+                                          public void onSwipeRight() {
+                                              Toast.makeText(MainActivity.this, "derecha", Toast.LENGTH_SHORT).show();
+                                          }
+
+                                          public void onSwipeLeft() {
+                                              Toast.makeText(MainActivity.this, "izquierda", Toast.LENGTH_SHORT).show();
+                                          }
+
+                                          public void onSwipeBottom() {
+                                              Toast.makeText(MainActivity.this, "abajo", Toast.LENGTH_SHORT).show();
+                                          }
+                                      }
+
+
+        );
     }
 
     @Override
@@ -76,25 +106,25 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-        Fragment fragment=null;
-        String titulo="";
+        Fragment fragment = null;
+        String titulo = "";
 
         if (id == R.id.nav_verListaEntrada) {
-            Intent intent=new Intent(MainActivity.this,ListaActivity.class);
+            Intent intent = new Intent(MainActivity.this, ListaActivity.class);
             startActivity(intent);
             //fragment=new VerListaFragment();
             //titulo="Ver Lista";
             // Handle the camera action
         }
         //else if (id == R.id.nav_verListaSalida) {
-            //Intent intent=new Intent(MainActivity.this,Lista2Activity.class);
-            //startActivity(intent);
-            //fragment=new VerListaFragment();
-            //titulo="Ver Lista";
-            // Handle the camera action
+        //Intent intent=new Intent(MainActivity.this,Lista2Activity.class);
+        //startActivity(intent);
+        //fragment=new VerListaFragment();
+        //titulo="Ver Lista";
+        // Handle the camera action
         //}
         else if (id == R.id.nav_entrada) {
-            Intent intent=new Intent(MainActivity.this,EntradaActivity.class);
+            Intent intent = new Intent(MainActivity.this, EntradaActivity.class);
             startActivity(intent);
 
             //fragment=new EntradaFragment();
@@ -102,12 +132,12 @@ public class MainActivity extends AppCompatActivity
 
         } //else if (id == R.id.nav_salida) {
 
-            //Intent intent=new Intent(MainActivity.this,SalidaActivity.class);
-            //startActivity(intent);
-            //fragment=new SalidaFragment();
-            //titulo="Marcar Salida";}
+        //Intent intent=new Intent(MainActivity.this,SalidaActivity.class);
+        //startActivity(intent);
+        //fragment=new SalidaFragment();
+        //titulo="Marcar Salida";}
         //else if(id==R.id.nav_cerrar_sesion){
-            //Eliminamos la sesion del sharedPreferences
+        //Eliminamos la sesion del sharedPreferences
           /*  SharedPreferences.Editor editor=getSharedPreferences("class3",MODE_PRIVATE).edit();
             editor.clear();
             editor.apply();
